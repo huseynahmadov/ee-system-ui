f789fa33c1d910d94d9d894fad01aba020bb3d40
huseynahmadov <t.huseyn96@mail.ru>
2022-06-17 01:08:39 +0400

    done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
-import AddEmployee from './components/AddEmployee';
-import EmployeeList from './components/EmployeeList';
-import Navbar from './components/Navbar';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import AddEmployee from "./components/AddEmployee";
+import EmployeeList from "./components/EmployeeList";
+import Navbar from "./components/Navbar";
+import UpdateEmployee from "./components/UpdateEmployee";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Navbar />
-
         <Routes>
-          <Route exact element={<EmployeeList />} />
-          <Route path='/' element={<EmployeeList />} />
-          <Route path='/employeeList' element={<EmployeeList />} />
-          <Route path='/addEmployee' element={<AddEmployee />} />
-          
+          <Route index element={<EmployeeList />} />
+          <Route path="/" element={<EmployeeList />}></Route>
+          <Route path="/employeeList" element={<EmployeeList />} />
+          <Route path="/addEmployee" element={<AddEmployee />} />
+          <Route path="/editEmployee/:id" element={<UpdateEmployee />} />
         </Routes>
-
       </BrowserRouter>
     </>
-
   );
 }
 
